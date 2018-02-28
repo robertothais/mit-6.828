9a1809da671777bc3e5ebfddee597eba46e30b53
Roberto Thais <r@robertothais.org>
2018-02-27 19:17:54 -0500

    Print octal numbers

--- lib/printfmt.c
+++ lib/printfmt.c
@@ -196,9 +196,9 @@ void vprintfmt(void (*putch)(int, void *), void *putdat, const char *fmt,
         // (unsigned) octal
         case 'o':
             // Replace this with your code.
-            putch('X', putdat);
-            putch('X', putdat);
-            putch('X', putdat);
+            num = getint(&ap, lflag);
+            base = 8;
+            goto number;
             break;
 
         // pointer
